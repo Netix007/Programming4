9438ab88f145347ab18db80fa4810e7abe473f8d
Alexandr <kino_mac@mail.ru>
2022-02-15 14:38:45 +0300

    Добавлено решение 1-ой задачи

---
+++  Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 26: Возведите число А в натуральную степень B используя цикл
+
+Random randomizer = new Random();
+int numberA = randomizer.Next(1,11);
+int numberB = randomizer.Next(1,11);
+double result = 1;
+
+for (int i = 1; i <= numberB; i++)
+{
+    result = result * numberA;
+}
+Console.WriteLine($"{numberA}^{numberB} = {result}");
