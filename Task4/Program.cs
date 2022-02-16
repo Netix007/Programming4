ccc9d70cb881f0a1065eee4989cbe692e83a4a62
Aleksandr <kino_mac@mail.ru>
2022-02-17 00:48:19 +0300

    Добавлено решение 4-ой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 32: Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+Random randomizer = new Random();
+int[] resultArray = new int[8];
+for (int i = 0; i <= 7; i++)
+{
+    resultArray[i] = randomizer.Next(0, 2);
+    Console.Write($"{resultArray[i]}  ");
+}
