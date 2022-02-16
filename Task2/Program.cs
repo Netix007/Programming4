f5c96a8da52b2d6dfa007142939ee6baadcc404b
Aleksandr <kino_mac@mail.ru>
2022-02-16 23:03:32 +0300

    Добавлено решение 2-ой задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 28: Подсчитать сумму цифр в числе
+
+int SumOfDigits(int number) // Метод "Вычисление суммы цифр в числе"
+{
+    int sum = 0;
+    for (int i = 1; number / i >= 1; i *= 10)
+    {
+        sum = sum + ((number / i) % 10);
+    }
+    return sum;
+}
+
+int number = new Random().Next(0,10000);
+int sum = SumOfDigits(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
